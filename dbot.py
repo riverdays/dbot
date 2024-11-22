4dfa31e5874f8defaccdf2aa1dcd15243a700f61
riverdays <riodiaz01@aol.com>
2024-11-22 09:54:24 -0500

    Update dbot.py

--- dbot.py
+++ dbot.py
@@ -116,4 +116,11 @@ async def nap(interaction: discord.Interaction):
 #Rook learned / commands!
 #""")
 
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv()
+
 client.run('BOT_TOKEN')
